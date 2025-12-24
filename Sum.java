cc72e4140e09a96cb556b3087e817d159b7e8d5f
kommireddyneelamrutha-4983 <kommireddyneelamrutha4983@gmail.com>
2025-12-24 10:00:09 +0530

    Implement a simple addition program in Java

---
+++ Sum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Sum{
+public static void main(String[]args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the number 1");
+int a=sc.nextInt();
+System.out.println("enter the number 2");
+int b =sc.nextInt();
+ int sum=a+b;
+System.out.println("the addition of number 1 & number 2 is :" +sum);
+}}
